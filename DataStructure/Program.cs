198bb469ebc5c9b1913078a1016e31623396042d
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-30 04:09:26 +0530

    [Vidya] Add : Creation And Display Linked List Of Three Elements

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                                 WELCOME TO DATA STRUCTURE PROGRAMS                                 \n");
+            Console.WriteLine("LINKED LIST -->\n");
+            LinkedListOperation list = new LinkedListOperation();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
