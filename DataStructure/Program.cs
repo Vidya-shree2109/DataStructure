a43656ed1d7208eebdedc350e44af64d6cb52ad1
Vidyashree N M <vidyashree2109@gmail.com>
2022-05-03 01:25:39 +0530

    [Vidya] Refactored : Deleting The Recently Added Element In The Linked List

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -11,7 +11,7 @@ namespace DataStructure
             Console.WriteLine("LINKED LIST -->\n");
             LinkedListOperation list = new LinkedListOperation();
             bool check = true;
-            Console.WriteLine("1.Simple Linked List\n2.Add in reverse order\n3.Append Data\n4.Insert Data\n5.Delete Data\n6.Delete Last Element\n7.Searching An Element\n8.Inserting New Element In The List\n9.Exit");
+            Console.WriteLine("1.Simple Linked List\n2.Add in reverse order\n3.Append Data\n4.Insert Data\n5.Delete Data\n6.Delete Last Element\n7.Searching An Element\n8.Inserting New Element In The List\n9.Delete The Recently Added Element\n10.Exit");
             while (check)
             {
                 Console.Write("\nEnter an option to execute : ");
@@ -67,6 +67,14 @@ namespace DataStructure
                         list.Display();
                         break;
                     case 9:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(40);
+                        list.Add(70);
+                        list.DeleteNodeAtParticularPosition(2);
+                        list.Display();
+                        break;
+                    case 10:
                         check = false;
                         break;
                 }
